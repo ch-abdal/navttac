84824ac5daf5bff7730c037dfb1e1d48624e095b
ch-abdal <chabdal7@gmail.com>
2026-02-17 08:43:02 -0700

    2nd push

--- pakistan.py
+++ pakistan.py
@@ -1,7 +1,7 @@
 cities=["lahore","multan","karachi","islamabd","kasor"]
 city=input("enter your city name:  ").lower()
 compelte=[ "city is clean"for i in cities if city==i]
-print(compelte) 
+#print(compelte) 
 
   
 
